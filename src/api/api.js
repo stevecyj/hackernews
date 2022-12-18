6ec7110540b90e617a1724c996137bc070a7c76c
Steve <ycsteven.tsao@gmail.com>
2022-12-18 10:43:51 +0800

    fix: access api

---
+++ src/api/api.js
@@ -0,0 +1,25 @@
+const databaseUrl = "https://hacker-news.firebaseio.com/v0";
+
+// const api = Firebase.database().ref("/v0");
+
+export function fetchListData(type) {
+  let res = fetchIdsByType(type)
+    .then((response) => response.json())
+    .then((ids) => fetchItems(ids));
+  console.log("==fetchListData==", res);
+  // return res;
+}
+
+export function fetchIdsByType(type) {
+  return fetch(`${databaseUrl}/${type}stories.json`);
+  // .then((data) => console.log(data));
+}
+
+export function fetchItem(id) {
+  return fetch(`${databaseUrl}/item/${id}.json`);
+}
+
+export function fetchItems(ids) {
+  console.log("ids", ids);
+  return Promise.all(ids.map((id) => fetchItem(id)));
+}
--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import { fetchListData } from "@/api/api";
+
 Vue.config.productionTip = false;
+
+fetchListData("top");
 
 new Vue({
   router,
